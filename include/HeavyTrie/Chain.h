11f565a164ac28773fb466a6745b23fea92d0ab6
unknown <arif.ozturk.ichc@gmail.com>
2023-05-09 20:43:53 +0100

    created chain class

---
+++ include/HeavyTrie/Chain.h
@@ -0,0 +1,29 @@
+#ifndef CHAIN_H
+#define CHAIN_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Chain {
+    public:
+        Chain(vector<char> values) {
+            this -> values = values;
+        }
+
+        string get_match_from_word(string word) {
+            int max_match_length = min(word.length(), values.size());
+            
+            for(int i = 0; i < max_match_length; i++) {
+                if(word[i] != values[i]) 
+                    return word.substr(0, i);
+            }
+            return word.substr(0, max_match_length);
+        }
+
+private:
+    vector<char> values;
+};
+
+#endif // CHAIN_H
