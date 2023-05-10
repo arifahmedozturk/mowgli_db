8eb6eabe6cae5a94bee731b54cc63b42fcb43d39
unknown <arif.ozturk.ichc@gmail.com>
2023-05-10 12:25:16 +0100

    added test

--- test/include/HeavyTrie/Chain.cpp
+++ test/include/HeavyTrie/Chain.cpp
@@ -23,6 +23,15 @@ TEST(ChainTest, creates_chain_correctly_from_numbers) {
     EXPECT_EQ(c.get_values(), values);
 }
 
+TEST(ChainTest, gets_match_from_chain_correctly) {
+    vector<char> values = {'a', 'b', 'a', 'c', 'd', 'a'};
+    Chain c(values);
+
+    EXPECT_EQ(c.get_match_from_word("abad"), "aba");
+    EXPECT_EQ(c.get_match_from_word("abacdada"), "abacda");
+    EXPECT_EQ(c.get_match_from_word("bad"), "");
+}
+
 TEST(ChainTest, writes_chain_to_file_correctly) {
     vector<char> values = {'a', 'b', 'c'};
     Chain c(values);
